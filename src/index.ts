56e1deaac866d76260dfa853e6ca282cba709a19
jbpkp07 <jeremy.w.barnes@gmail.com>
2021-12-18 13:57:04 -0800

    Updated a few packages

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,35 @@
-export const sum = (a: number, b: number) => {
-    return a + b;
+import { watch } from "fs/promises";
+
+const watchDirectory = async (dir: string) => {
+    try {
+        const watcher = watch(dir, { recursive: true });
+
+        console.log("here");
+        // eslint-disable-next-line no-restricted-syntax
+        for await (const event of watcher) {
+            console.log(event);
+        }
+    } catch (error) {
+        console.log(error);
+    }
 };
 
-console.log("Hello World!");
+// import { watch, WatchEventType } from "fs";
+
+// console.log("here\n\n");
+
+// const watchDirectory = (dir: string) => {
+//     const watcher = watch(dir, { recursive: true });
+
+//     const listener = (a: WatchEventType, b: string) => {
+//         // watcher.off("change", listener);
+//         console.log(a);
+//         console.log(b);
+//         // watcher.on("change", listener);
+//     };
+
+//     watcher.on("change", listener);
+//     // console.log(cool);
+// };
+
+void watchDirectory("C:/AAA/watch");
--- test/sum.test.ts
+++ test/sum.test.ts
@@ -1,5 +1,6 @@
-import { sum } from "../src/index";
+/* eslint-disable jest/no-commented-out-tests */
+// import { sum } from "../src/index";
 
-test("adds 1 + 2 to equal 3", () => {
-    expect(sum(1, 2)).toBe(3);
-});
+// test("adds 1 + 2 to equal 3", () => {
+//     expect(sum(1, 2)).toBe(3);
+// });
